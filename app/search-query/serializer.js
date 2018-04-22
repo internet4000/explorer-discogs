cbe51033eb026f72e2780f73faccdc70904b9859
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2018-04-22 19:09:34 +0200

    clean search-query serializer

--- app/search-query/serializer.js
+++ app/search-query/serializer.js
@@ -1,26 +1,5 @@
 import DS from 'ember-data';
 import ApplicationSerializer from '../application/serializer';
 
-const serializeSearch = (item, index) => {
-  item.resultType = item.type;
-  item.type = 'search-query';
-  return item;
-};
-
 export default ApplicationSerializer.extend({
-  // normalizeArrayResponse (store, primaryModelClass, payload, id, requestType) {
-  //   payload = {
-  //     data: payload.results.map(serializeSearch),
-  //     meta: {
-  //       pagination: payload.pagination
-  //     },
-  //     errors: payload.errors || []
-  //   };
-  //   delete payload.results;
-  //   delete payload.pagination;
-  //   delete payload.errors;
-
-  //   // return this._super(...arguments);
-  //   return payload;
-  // }
 });
