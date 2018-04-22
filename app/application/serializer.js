0b62238e02a94baef8497223d53a376a399d473a
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2018-04-22 19:07:21 +0200

    feat: clean applicatio nserializer

--- app/application/serializer.js
+++ app/application/serializer.js
@@ -4,22 +4,5 @@ export default DS.JSONSerializer.extend({
   normalizeResponse(store, primaryModelClass, payload, id, requestType) {
     let newPayload = payload.results;
     return this._super(store, primaryModelClass, newPayload, id, requestType);
-  },
-  // normalizeResponse(store, primaryModelClass, payload, id, requestType) {
-  //   payload = {
-  //     data: payload.results,
-  //     meta: {
-  //       pagination: payload.pagination
-  //     }
-  //   };
-
-  //   delete payload.results;
-  //   delete payload.pagination;
-
-  //   return this._super(...arguments);
-  // },
-
-  normalize(modelClass, resourceHash) {
-    return this._super(...arguments);
   }
 });
