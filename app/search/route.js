6baa9de939b80914123a5b7fe4f04567a7031b37
Oskar Rough <oskar@rough.dk>
2018-04-22 21:28:51 +0200

    Add links to search template

--- app/search/route.js
+++ app/search/route.js
@@ -2,9 +2,8 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   model() {
-    let search = this.get('store').query('searchQuery', {
+    return this.get('store').query('searchQuery', {
       q: 'nirvana'
     });
-    return search;
   }
 });
