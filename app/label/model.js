12468388d865f31e9a85728f2e5a0ba3fe37c4ec
Oskar Rough <oskar@rough.dk>
2018-04-22 15:40:50 +0200

    Tweak label page

--- app/label/model.js
+++ app/label/model.js
@@ -1,7 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  contactInfo: DS.attr('string'),
+  contact_info: DS.attr('string'),
+  releases_url: DS.attr('string'),
   // images: DS.attr(),
   name: DS.attr('string'),
   profile: DS.attr('string'),
