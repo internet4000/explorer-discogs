2cce96db5d5f90199dacf8bfa23dcbe7d30b1282
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2018-04-22 19:04:41 +0200

    workingstage

--- app/application/adapter.js
+++ app/application/adapter.js
@@ -1,7 +1,7 @@
 import DS from 'ember-data';
 import { computed } from '@ember/object';
 
-export default DS.JSONAPIAdapter.extend({
+export default DS.RESTAdapter.extend({
   host: 'https://api.discogs.com',
   headers: computed(function() {
     return {
--- app/application/serializer.js
+++ app/application/serializer.js
@@ -1,4 +1,25 @@
 import DS from 'ember-data';
 
-export default DS.JSONAPISerializer.extend({
+export default DS.JSONSerializer.extend({
+  normalizeResponse(store, primaryModelClass, payload, id, requestType) {
+    let newPayload = payload.results;
+    return this._super(store, primaryModelClass, newPayload, id, requestType);
+  },
+  // normalizeResponse(store, primaryModelClass, payload, id, requestType) {
+  //   payload = {
+  //     data: payload.results,
+  //     meta: {
+  //       pagination: payload.pagination
+  //     }
+  //   };
+
+  //   delete payload.results;
+  //   delete payload.pagination;
+
+  //   return this._super(...arguments);
+  // },
+
+  normalize(modelClass, resourceHash) {
+    return this._super(...arguments);
+  }
 });
