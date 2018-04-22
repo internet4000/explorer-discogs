fb448d293f45fc60501a639fa4607a54709ca951
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2018-04-22 19:25:21 +0200

    find right hook for search, that does not brake the single response

--- app/application/serializer.js
+++ app/application/serializer.js
@@ -1,7 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.JSONSerializer.extend({
-  normalizeResponse(store, primaryModelClass, payload, id, requestType) {
+  normalizeArrayResponse(store, primaryModelClass, payload, id, requestType) {
     let newPayload = payload.results;
     return this._super(store, primaryModelClass, newPayload, id, requestType);
   }
