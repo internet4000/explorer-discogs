3acab2a1e408cbe4a136e84a948d850cfdfe464a
Oskar Rough <oskar@rough.dk>
2018-04-23 16:33:59 +0200

    Replace ember-ajax with ember-fetch

--- app/application/adapter.js
+++ app/application/adapter.js
@@ -1,7 +1,8 @@
 import DS from 'ember-data';
 import { computed } from '@ember/object';
+import AdapterFetch from 'ember-fetch/mixins/adapter-fetch'
 
-export default DS.RESTAdapter.extend({
+export default DS.RESTAdapter.extend(AdapterFetch, {
   host: 'https://api.discogs.com',
 
   // Querying Discogs "search" endpoints requires authentication.
