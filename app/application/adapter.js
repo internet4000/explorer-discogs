a5a6cef8e5ed3dca9bd5941e8abe6a64340a6077
Oskar Rough <oskar@rough.dk>
2018-04-23 16:34:39 +0200

    Only send auth headers on search route

--- app/application/adapter.js
+++ app/application/adapter.js
@@ -1,15 +1,6 @@
 import DS from 'ember-data';
-import { computed } from '@ember/object';
 import AdapterFetch from 'ember-fetch/mixins/adapter-fetch'
 
 export default DS.RESTAdapter.extend(AdapterFetch, {
-  host: 'https://api.discogs.com',
-
-  // Querying Discogs "search" endpoints requires authentication.
-  // https://www.discogs.com/developers/#page:authentication,header:authentication-discogs-auth-flow
-  headers: computed(function() {
-    return {
-      'Authorization': 'Discogs key=cnoOBdbCifZbuXMwNtph, secret=VzlAmAOVmCjeLFjXlFXZwSYYRarmJHJR'
-    };
-  })
+  host: 'https://api.discogs.com'
 });
