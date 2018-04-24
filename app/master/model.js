01d0f6fdca39a1f7133d91003fd2d1eb134ec949
Oskar Rough <oskar@rough.dk>
2018-04-24 15:26:21 +0200

    Define "master" model and create a template

--- app/master/model.js
+++ app/master/model.js
@@ -1,5 +1,21 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-
+  styles: DS.attr(),
+  genres: DS.attr(),
+  mainReleaseUrl: DS.attr('string'),
+  numForSale: DS.attr('number'),
+  videos: DS.attr(),
+  title: DS.attr('string'),
+  mainRelease: DS.belongsTo('release'),
+  // artists: DS.hasMany('artist'),
+  artists: DS.attr(),
+  uri: DS.attr('string'),
+  versionsUrl: DS.attr('string'),
+  lowestPrice: DS.attr('number'),
+  year: DS.attr('number'),
+  images: DS.attr(),
+  resourceUrl: DS.attr('string'),
+  dataQuality: DS.attr('string'),
+  tracklist: DS.attr()
 });
