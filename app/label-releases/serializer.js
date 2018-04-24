e9f0cecb7f13944c4f7395c52d9f29597909f45d
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2018-04-24 22:01:34 +0200

    list label releases

--- app/label-releases/serializer.js
+++ app/label-releases/serializer.js
@@ -2,10 +2,18 @@ import DS from 'ember-data';
 import ReleaseSerializer from '../release/serializer';
 
 export default ReleaseSerializer.extend({
+  attr: {
+    releases: {
+      embedded: 'always'
+    }
+  },
   normalizeResponse(store, primaryModelClass, payload, id, requestType) {
     let newPayload = {
       id,
-      releases: payload.releases
+      releases: payload.releases.map((i) => {
+        i.type = 'release';
+        return i;
+      })
     };
 
     return this._super(store, primaryModelClass, newPayload, id, requestType);
