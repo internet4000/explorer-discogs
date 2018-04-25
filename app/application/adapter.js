56267d971bb73d2dbe502158098bfca0affb3e9e
Oskar Rough <oskar@rough.dk>
2018-04-25 21:16:35 +0200

    Use proxy

--- app/application/adapter.js
+++ app/application/adapter.js
@@ -2,7 +2,8 @@ import DS from 'ember-data';
 import AdapterFetch from 'ember-fetch/mixins/adapter-fetch'
 
 export default DS.RESTAdapter.extend(AdapterFetch, {
-  host: 'https://api.discogs.com',
+  // host: 'https://api.discogs.com',
+  host: 'https://edapi.glitch.me',
 
   // Don't fetch records again if they are already in the store.
   shouldBackgroundReloadRecord() {
--- app/search-query/adapter.js
+++ app/search-query/adapter.js
@@ -4,7 +4,11 @@ import { computed } from '@ember/object';
 const Authorization = 'Discogs key=cnoOBdbCifZbuXMwNtph, secret=VzlAmAOVmCjeLFjXlFXZwSYYRarmJHJR'
 
 export default ApplicationAdapter.extend({
+  // Don't use proxy for search. Not ready yet.
+  host: 'https://api.discogs.com',
+
   namespace: 'database',
+
   pathForType() {
     return 'search';
   },
