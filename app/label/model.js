2ceb97a5f6bd62a8bf971a783aec93821618b0ad
Oskar Rough <oskar@rough.dk>
2018-04-25 22:29:32 +0200

    Tweak menu styles

--- app/label/model.js
+++ app/label/model.js
@@ -2,15 +2,14 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   contactInfo: DS.attr('string'),
-  releasesUrl: DS.attr('string'),
   images: DS.attr(),
   name: DS.attr('string'),
   profile: DS.attr('string'),
+  releasesUrl: DS.attr('string'),
   uri: DS.attr('string'),
   urls: DS.attr(),
 
-  sublabels: DS.hasMany('label', {inverse: null, async: true}),
-  parentLabel: DS.belongsTo('label', {inverse: null, async: true}),
-
   labelReleases: DS.belongsTo('labelReleases', {inverse: null, async: true}),
+  parentLabel: DS.belongsTo('label', {inverse: null, async: true}),
+  sublabels: DS.hasMany('label', {inverse: null, async: true}),
 });
