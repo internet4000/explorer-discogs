b5e17ed03e2611a5f1a18aabf31389cdfa1ab8a5
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2018-04-25 23:39:10 +0200

    add player 0.0.1

--- app/components/youtube-player/component.js
+++ app/components/youtube-player/component.js
@@ -1,9 +1,22 @@
-import 'npm:yt-player';
+import YTPlayer from 'npm:yt-player';
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
 import { alias } from '@ember/object/computed';
 
 export default Component.extend({
   player: service(),
-  currentTrack: alias('player.currentTrack')
+  youtubePlayer: alias('player.youtubePlayer'),
+  currentTrack: alias('player.currentTrack'),
+  didInsertElement() {
+    this._super(...arguments);
+
+    const playerDom = this.element.querySelector('#Player');
+    const player = new YTPlayer(playerDom);
+
+    this.set('youtubePlayer', player);
+
+    player.on('playing', () => {
+      console.log(this.get('youtubePlayer').getDuration());
+    });
+  }
 });
--- app/player/service.js
+++ app/player/service.js
@@ -1,5 +1,20 @@
 import Service from '@ember/service';
+import youtubeRegex from 'npm:youtube-regex';
+
+const youtubeIdFromUrl = (url) => {
+  const results = youtubeRegex().exec(url);
+	if (!results) {
+		return false;
+	}
+	return results[1];
+};
 
 export default Service.extend({
-  currentTrack: undefined
+  currentTrack: undefined,
+  youtubePlayer: undefined,
+  play(model) {
+    this.get('youtubePlayer')
+      .load(youtubeIdFromUrl(model.uri), true);
+    this.set('currentTrack', model);
+  }
 });
