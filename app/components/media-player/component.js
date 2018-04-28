c04c948bfc06e89fdfa6d8b41e3c49cc33dd73c3
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2018-04-28 17:58:52 +0200

    chore: simplify player.mediaPlayer assignation

--- app/components/media-player/component.js
+++ app/components/media-player/component.js
@@ -1,13 +1,11 @@
 import Component from '@ember/component';
 import Radio4000 from 'npm:radio4000-player';
 import { inject as service } from '@ember/service';
-import { alias } from '@ember/object/computed';
 
 export default Component.extend({
   tagName: 'div',
   classNames: ['MediaPlayer'],
   player: service(),
-  mediaPlayer: alias('player.mediaPlayer'),
   didInsertElement() {
     this._super(...arguments);
 
@@ -15,7 +13,7 @@ export default Component.extend({
 
     // get the js instance of the <radio4000-player> dom el
     let vue = playerDom.__vue_custom_element__.$children[0];
-    this.set('mediaPlayer', vue);
+    this.set('player.mediaPlayer', vue);
 
     // vue.updatePlaylist(playlist);
   }
