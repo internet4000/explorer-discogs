bc9f72e195c5ff6ccc8d17a9dd1e92521ecc5776
Hugo Vieilledent <hugo.vieilledent@gmail.com>
2018-04-29 13:23:08 +0200

    feat: add {{resource-head}} to display a resource title

--- app/components/link-to-discogs/component.js
+++ app/components/link-to-discogs/component.js
@@ -2,6 +2,7 @@ import LinkToExternal from '../link-to-external/component';
 import layout from '../link-to-external/template';
 
 export default LinkToExternal.extend({
-  text: 'View on Discogs',
+  url: undefined,
+  text: 'Discogs',
   layout
 });
