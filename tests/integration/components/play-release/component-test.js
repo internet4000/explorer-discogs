be569c6c113b853f5299efc67557d53912092c97
Oskar Rough <oskar@rough.dk>
2018-04-30 15:13:45 +0200

    Fix tests

--- tests/integration/components/play-release/component-test.js
+++ tests/integration/components/play-release/component-test.js
@@ -7,20 +7,7 @@ module('Integration | Component | play-release', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
     await render(hbs`{{play-release}}`);
-
-    assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
-    await render(hbs`
-      {{#play-release}}
-        template block text
-      {{/play-release}}
-    `);
-
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    assert.ok(this.element.textContent.includes('Play'))
   });
 });
