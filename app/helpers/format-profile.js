707fcf89cc6f3498bb73f66a3257459978360d60
Oskar Rough <oskar@rough.dk>
2018-05-18 14:30:01 +0200

    Update name

--- app/helpers/format-profile.js
+++ app/helpers/format-profile.js
@@ -1,11 +1,11 @@
 import {helper} from '@ember/component/helper'
 
 const hrefRegex = /href="(.*?)"/gm
-const typeAndIdRegex = /([a-z]+(?:\/))([0-9]+(?:$|(?=[?-])|(?=\/$)))/gm
+const typeAndIdFromUrlRegex = /([a-z]+(?:\/))([0-9]+(?:$|(?=[?-])|(?=\/$)))/gm
 
 export function formatProfile([profile]) {
   return profile.replace(hrefRegex, (href) => {
-    let [typeAndId] = href.match(typeAndIdRegex)
+    let [typeAndId] = href.match(typeAndIdFromUrlRegex)
     const [type, id] = typeAndId.split('/')
     return `href="/${type}s/${id}"`
   })
