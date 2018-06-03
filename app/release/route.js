4c66757d7bf5fc4c005501e9b15506328a18775f
Oskar Rough <oskar@rough.dk>
2018-06-03 18:23:16 +0200

    Reload release model if needed

--- app/release/route.js
+++ app/release/route.js
@@ -1,4 +1,10 @@
-import Route from '@ember/routing/route';
+import Route from '@ember/routing/route'
 
 export default Route.extend({
-});
+  afterModel(model) {
+    // When a model comes from an artist/label release relationship,
+    // it doesn't contain all properties.
+    let isMissingProperties = !model.get('uri')
+    if (isMissingProperties) return model.reload()
+  }
+})
