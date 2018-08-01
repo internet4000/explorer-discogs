9d814a6d0651891e32ef47a4d3c7114e836aee24
Oskar Rough <oskar@rough.dk>
2018-08-01 12:03:19 +0200

    Fix tests

--- tests/unit/artist/serializer-test.js
+++ tests/unit/artist/serializer-test.js
@@ -1,24 +1,25 @@
-import { module, test } from 'qunit';
-import { setupTest } from 'ember-qunit';
-import { run } from '@ember/runloop';
+import {module, test} from 'qunit'
+import {setupTest} from 'ember-qunit'
+import {run} from '@ember/runloop'
 
 module('Unit | Serializer | artist', function(hooks) {
-  setupTest(hooks);
+  setupTest(hooks)
 
   // Replace this with your real tests.
   test('it exists', function(assert) {
-    let store = this.owner.lookup('service:store');
-    let serializer = store.serializerFor('artist');
+    let store = this.owner.lookup('service:store')
+    let serializer = store.serializerFor('artist')
 
-    assert.ok(serializer);
-  });
+    assert.ok(serializer)
+  })
 
   test('it serializes records', function(assert) {
-    let store = this.owner.lookup('service:store');
-    let record = run(() => store.createRecord('artist', {}));
+    let store = this.owner.lookup('service:store')
+    let record = run(() => store.createRecord('artist', {}))
 
-    let serializedRecord = record.serialize();
+    let serializedRecord = record.serialize()
+    console.log(serializedRecord)
 
-    assert.ok(serializedRecord);
-  });
-});
+    assert.ok(serializedRecord)
+  })
+})
